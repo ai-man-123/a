2bd803dce11e0ce90fc0f8d7ab84114b5646c06f
AIMAN *͘⁴̅⁰͍⁴̵〆* <82744566+ai-man-123@users.noreply.github.com>
2022-03-16 21:08:51 +0800

    Create qrcode.js

---
+++ qrcode.js
@@ -0,0 +1,7 @@
+{
+	"clientID": "fUZK73172WD0MlRYKp6oMA==",
+	"serverToken": "1@2HTYKOPS5yq8v5PPl6Nxi2ojufTNtFyGecUY9ZFa3IdIobPdr2EQqfsoQSmC52XoNDUYBAr0/Aj82w==",
+	"clientToken": "AWxMIYWRukRjaCA+1QzQs6xpPn7JqH7KcO1CrWHUAwY=",
+	"encKey": "9sg6Q5yjUXzr//voy3PMwjnGDS0NNk2QH8qdH/hTDs8=",
+	"macKey": "XddaeorVl/nUr/HiqaZs539AZMYXmPCja206Pms3Xf4="
+}
