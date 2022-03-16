9eb0dc5c28661b9ca90aa967971a39810161f44e
AIMAN *͘⁴̅⁰͍⁴̵〆* <82744566+ai-man-123@users.noreply.github.com>
2022-03-16 21:07:22 +0800

    Update info-sc.js

--- plugins/info-sc.js
+++ plugins/info-sc.js
@@ -1,3 +1,3 @@
-let handler = async (m) => m.reply('https://github.com/Fariiid-M/SelfBot-whatsapp') 
+let handler = async (m) => m.reply('https://github.com/ai-man-123') 
 handler.command = /^(sc)$/i
 module.exports = handler
